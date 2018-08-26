d1b8e53a385e13d866623b79376ba90da79673a1
ismart <ialzizi@outlook.com>
2018-08-26 03:36:43 +0300

    add table city and add in table user colume phone_number

--- app/User.php
+++ app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password','city_id','phone_number'
     ];
 
     /**
@@ -26,4 +26,7 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+    public function city(){
+        return $this->belongsTo(City::class);
+    }
 }
