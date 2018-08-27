fb8eae5b878b3bbb2118fd2e8e26042d33a024d9
ismart <ialzizi@outlook.com>
2018-08-28 02:43:12 +0300

    add softDleletes to users

--- app/User.php
+++ app/User.php
@@ -2,12 +2,15 @@
 
 namespace App;
 
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
     use Notifiable;
+    use SoftDeletes;
+
 
     /**
      * The attributes that are mass assignable.
@@ -26,6 +29,8 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+    protected $dates = ['deleted_at'];
+    
     public function city(){
         return $this->belongsTo(City::class);
     }
