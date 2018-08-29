bbda2f4bcbdd2fc11f74ab6b098abd6991d254a0
ismart <ialzizi@outlook.com>
2018-08-30 01:19:48 +0300

    add item city_id and phone number in registerController

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -78,8 +78,8 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
-            'email' => $data['email'],
-            'email' => $data['email'],
+            'city_id' => $data['city_id'],
+            'phone_number' => $data['phone_number'],
         ]);
     }
 
